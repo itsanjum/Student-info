91e47abbbbf344394e7bbe4eaf43ef08349a0da6
Anjum Unnisa <anjum@Pro.local>
2024-12-04 12:47:46 +0530

    Initial commit: Add Student.js

---
+++ Student.js
@@ -0,0 +1,10 @@
+const Student={
+    name :"Anjum",
+    phone_number :"8073801279",
+     marks : 79
+}
+
+console.log("Student Detail of "+Student["name"]);
+console.log("Name :" + Student.name);
+console.log("Phone_number :" + Student.phone_number);
+console.log("Marks :" + Student.marks);
